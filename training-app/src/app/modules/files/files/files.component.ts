7dde1e940e45598a3e1839ecf01ca84f5a046b44
vivekmm97 <viveknambiar0@gmail.com>
2020-09-20 17:47:13 +0530

    completed

--- training-app/src/app/modules/files/files/files.component.ts
+++ training-app/src/app/modules/files/files/files.component.ts
@@ -8,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class FilesComponent implements OnInit {
 
   constructor() { }
-
+	fileToUpload: File = null;
+  url :any;
   ngOnInit(): void {
   }
 uploadFile(val : any){
   console.log("File Uploaded");
 }
+
+onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      // tslint:disable-next-line: prefer-const
+      let reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      // tslint:disable-next-line: no-shadowed-variable
+      reader.onload = (event) => { // called once readAsDataURL is completed
+         this.url = event.target.result;
+      };
+    }
+  }
 }
